9a3d14579c362475ef5719a2ccbf036f6cf9341f
Ryan Ramdehol <rramdeholshift@gmail.com>
2016-12-17 13:57:10 -0500

    randomized placement of cards and got the flip to stay when matched

--- scripts.js
+++ scripts.js
@@ -1,13 +1,15 @@
 // AN ARRAY OF CARDS IMAGES
 var cards = [
+  '<img src="../blackjack/cards/4s.png">',
+  '<img src="../blackjack/cards/5s.png">',
 	'<img src="../blackjack/cards/6s.png">',
 	'<img src="../blackjack/cards/7s.png">',	
 	'<img src="../blackjack/cards/8s.png">',
 	'<img src="../blackjack/cards/9s.png">',
 	'<img src="../blackjack/cards/10s.png">',
 	'<img src="../blackjack/cards/11s.png">',
-    '<img src="../blackjack/cards/12s.png">',
-    '<img src="../blackjack/cards/13s.png">',
+  '<img src="../blackjack/cards/12s.png">',
+  '<img src="../blackjack/cards/13s.png">',
 ];
 // All code will wait until the DOM is ready!
 $(document).ready(function(){
@@ -35,23 +37,19 @@ $(document).ready(function(){
     //     else{
     //     	card = cards[5];
     //     }
-    var randomNumber = Math.floor(Math.random()*cards.length);
+    var matchCards = cards.concat(cards)
+    var gridSize = matchCards.length; 
+    var randomNumber = Math.floor(Math.random()*(matchCards.length-1));
 		for(let i=0;i<(gridSize);i++){
-		  console.log(randomNumber);
-			for(let j=0;j<=1;j++){
-				card = cards[randomNumber];
-				console.log(card);
+        var card = matchCards[randomNumber];
 				mgHTML += '<div class="mg-tile col-sm-2">';
-            		mgHTML += '<div class="mg-tile-inner">';
-                		mgHTML += '<div class="mg-front">'+card+'</div>';
-                		mgHTML += '<div class="mg-back"></div>';
-            		mgHTML += '</div>';
-        		mgHTML += '</div>';
-			}
-				cards.splice(randomNumber, 1);
-				var randomNumber = Math.floor(Math.random()*cards.length);
-			// 	console.log(i);
-				console.log(cards);
+            mgHTML += '<div class="mg-tile-inner">';
+              mgHTML += '<div class="mg-front">'+card+'</div>';
+              mgHTML += '<div class="mg-back"></div>';
+            mgHTML += '</div>';
+        mgHTML += '</div>';
+        matchCards.splice(randomNumber, 1);
+        var randomNumber = Math.floor(Math.random()*(matchCards.length-1));
 			}
         // mgHTML += '<div class="mg-tile col-sm-2">';
         //     mgHTML += '<div class="mg-tile-inner">';
@@ -59,30 +57,34 @@ $(document).ready(function(){
         //         mgHTML += '<div class="mg-back"></div>';
         //     mgHTML += '</div>';
         // mgHTML += '</div>';
-	console.log(mgHTML);
+	 console.log(mgHTML);
+   
    $('.mg-contents').html(mgHTML);
    $('.mg-tile-inner').click(function(){
        $(this).toggleClass('flip');
+       // STORE THE AMOUNT OF FLIP CLASSES TO A VARIABLE
+       var cardsUp = $(".flip");
+       // IF TWO CARDS HAVE THE FLIP CLASS 2 CARDS HAVE BEEN REVEALED 
+       if(cardsUp.length === 2){
+          // CHECK TO SEE IF THEY ARE SAME
+          var cardsUpImages = cardsUp.find(".mg-front img");
+          console.dir(cardsUpImages);
+          if(cardsUpImages[0].src === cardsUpImages[1].src){
+            // This is a match!!!!!
+            cardsUp.addClass("matched");
+            cardsUp.removeClass("flip");
+          }
+          else{
+            // THE USER HAS 2 CARDS AND THEY DONT MATCH
+            setTimeout(function(){
+              cardsUp.removeClass("flip");
+            },2000);
+          }
+       }
+       else{
+          // DO NOTHING
+       }
    })
-   var cardsUp = $(".flip");
-   if(cardsUp.length === 2){
-   		// CHECK TO SEE IF THEY ARE SAME
-   		var cardsUpImages = cardsUp.find(".mg-front img");
-   		if(cardsUpImages[0].src === cardsUpImages[0].src){
-   			// This is a match!!!!!
-   			cardsUp.addClass("matched");
-   			cardsUp.removeClass("flip");
-   		}
-   		else{
-   			// THE USER HAS 2 CARDS AND THEY DONT MATCH
-   			setTimeOut(function(){
-   				cardsUp.removeClass("flip");
-   			},2000);
-   		}
-   }
-   else{
-   		// DO NOTHING
-
-   }
+   
 
 });
